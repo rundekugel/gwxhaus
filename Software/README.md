b82197cf38072e5ff10b6c500ef138fe15d888bc
rundekugel <rundekugel@gmx.de>
2024-05-13 10:22:17 +0200

    more doc / mock up

---
+++ Software/README.md
@@ -0,0 +1,29 @@
+# gwxhaus
+## Greenhouse Controller Software
+
+*It's just started, not finished.*
+
+Open firmware to control a greenhouse heating and cooling. Depends on temperature, humidity, wind and time.
+
+An ESP32 controls the motor-controller and reads sensor data.
+
+### Sensors
+- Temperature in/out
+- Humidity in/out
+- Wind
+- Soil moisture
+
+There can be a lot of settings depending on sensor values and time.
+
+### Actors
+- Motors to open/close the windows
+- Valves to control water
+- Relais to control heating
+
+The ESP32 is connected via another ESP32, ESP8266 or RaspberryPi to the WWW. Via mqtt the sensor data and actor positions are stored in a database and can be viewd in charts.
+
+Optional, password protected, it's possible to change the settings and manually control the actors.
+
+If any sensor values are out of range, or power fails, there can be messages sent via email, telegram, SMS, telephone (voip), or other channels.
+
+Weather forecast data from Deutscher Wetterdienst https://dwd.api.bund.dev/ can be used to protect windows against storm or hail.
--- Software/uPython/HYT221.py
+++ Software/uPython/HYT221.py
@@ -1,4 +1,10 @@
-# upython read i2c
+# upython read i2c 
+"""
+read humidity and temperature from HYT-221 sensor via i2c
+written by rundekugel @github
+todo: put everything in one class
+"""
+
 
 from machine import SoftI2C as I2C
 from machine import Pin
@@ -6,17 +12,14 @@ import binascii
 import utime
 import struct
 
-# gpio: 0,4,5 ==> D3,D2,D1
-
 HYT_Addr = 0x28
 DHT10_Addr = 0x70
 
+# set used pins
+# gpio: 0,4,5 ==> D3,D2,D1 on ESP-12e
 p5=Pin(5,Pin.PULL_UP,Pin.OPEN_DRAIN)
 p4=Pin(4,Pin.PULL_UP,Pin.OPEN_DRAIN)
 
-#
-i2c=I2C(scl=p5, sda=p4, freq=int(1e3))
-i2c2=I2C(scl=p5, sda=p4, freq=int(1e3), timeout=int(10e3))
 
 def setOff():
     p4.off()
@@ -25,15 +28,8 @@ def setOff():
 def setOn():
     p4=Pin(4,Pin.PULL_UP,Pin.OUT)
     p4.on()
-    # p5=Pin(5,Pin.PULL_UP,Pin.OUT)
     p5.on()
     
-
-setOn()
-print(i2c)
-utime.sleep(.1)
-devs = i2c.scan()
-print("found:",devs, binascii.hexlify(bytes(devs)," "))
 
 class Result:
     status=None
@@ -57,7 +53,7 @@ def softreset(i2c=i2c):
     
 def getHYT(i2c=i2c, verbosity=1):
     """from: AHHYTM_E2.3.6"""
-    # setOn() # in case, it's switched off during pause
+    setOn() # in case, it's switched off during pause
     utime.sleep(.15)
     i2c.readfrom_mem(0x28,0x1c,2)  # needed to initialize the next data measuring
     utime.sleep(.01)
@@ -70,13 +66,24 @@ def getHYT(i2c=i2c, verbosity=1):
     h<<=8
     h|=r[1]
     h*=(100/(2**14-1))
-    # t=((r[2]&0b11111100))<<6 | (r[3]>>2)
     t=(r[2]<<6) | (r[3]>>2)
     t*=(165/16383)
     t-=40
     return Result(status,h,t)
 
+
+# init i2c interface
+i2c=I2C(scl=p5, sda=p4, freq=int(1e3))
+# i2c2=I2C(scl=p5, sda=p4, freq=int(1e3), timeout=int(10e3))
+setOn() 
+
+def scan(i2ce=i2c):
+    devs = i2c.scan()
+    print("I2C devices found: ",devs, " = ", binascii.hexlify(bytes(devs)," "))
+    return devs
+    
 if __name__ == "__main__":
+    scan()
     hyt = getHYT(i2c, verbosity=0)
     print("Status, Luftfeuchte, Temperatur: ",hyt.status, hyt.hygro, hyt.thermo)
     # or simply:
