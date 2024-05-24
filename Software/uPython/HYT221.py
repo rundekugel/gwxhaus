f4ed5b7738c17cb25d3290b61b68a6fc46073e19
rundekugel <rundekugel@gmx.de>
2024-05-25 00:22:22 +0200

    HYT221 changed to class, to support multiple sensors

--- Software/uPython/HYT221.py
+++ Software/uPython/HYT221.py
@@ -1,10 +1,9 @@
-# upython read i2c 
+# HYT221
 """
-read humidity and temperature from HYT-221 sensor via i2c
-written by rundekugel @github
-todo: put everything in one class
+Runs with esp-32 and uPython.
+Read humidity and temperature from HYT-221 sensor via i2c
+Written by rundekugel @github 2024
 """
-
 
 from machine import SoftI2C as I2C
 from machine import Pin
@@ -12,85 +11,91 @@ import binascii
 import utime
 import struct
 
-__version__ = "0.9.1"
+__version__ = "1.0.0"
 
 HYT_Addr = 0x28
 DHT10_Addr = 0x70
+FREQ_DEFAULT = int(1e3)
 
-# set used pins
-# gpio: 0,4,5 ==> D3,D2,D1 on ESP-12e
-p5=Pin(5,Pin.PULL_UP,Pin.OPEN_DRAIN)
-p4=Pin(4,Pin.PULL_UP,Pin.OPEN_DRAIN)
-# i2c = None
-i2c=I2C(scl=p5, sda=p4, freq=int(1e3))
-
-def setOff():
-    p4.off()
-    p5.off()
-
-def setOn():
-    p4=Pin(4,Pin.PULL_UP,Pin.OUT)
-    p4.on()
-    p5.on()
-    
 
 class Result:
     status=None
-    hygro=None
-    thermo=None
+    humidity=None
+    temperature=None
+    verbosity = 0
     
     def __init__(self,s,h,t):
-        self.status, self.hygro, self.thermo = s,h,t
+        self.status, self.humidity, self.temperature = s,h,t
     
-    def toString(self):
-        return "Status, Luftfeuchte, Temperatur: "+str(self.status)+"/"+str(self.hygro)+"/"+str(self.thermo)
     def __str__(self):
-        return "Status, Luftfeuchte, Temperatur: "+str(self.status)+"/"+str(self.hygro)+"/"+str(self.thermo)
+        return ("Status, Luftfeuchte, Temperatur: "
+                +str(self.status)+"/"+str(self.humidity)+"/"+str(self.temperature))
     def __unicode__(self):
-        return "Status, Luftfeuchte, Temperatur: "+str(self.status)+"/"+str(self.hygro)+"/"+str(self.thermo)
+        return ("Status, Luftfeuchte, Temperatur: "
+                +str(self.status)+"/"+str(self.humidity)+"/"+str(self.temperature))
     
-def softreset(i2c=i2c):
-    # todo
-    i2c.readfrom_mem(0x28,0x1c,2)  # needed to initialize the next data sampling
-    return
+class HYT221:
+    """
+    read humidity, temperature and status from HYT221-sensor
+    params: pins as object from Pin or as integer
+    """
+    i2c = None
+    address = None
+    verbosity = 0
+
+    def __init__(self, pinSCK, pinSDA, address=HYT_Addr,
+                 freq=FREQ_DEFAULT, verbosity=0):
+        self.verbosity = verbosity
+        if isinstance(pinSCK, int):
+            pinSCK = Pin(pinSCK,Pin.PULL_UP,Pin.OPEN_DRAIN)
+        if isinstance(pinSDA, int):
+            pinSDA = Pin(pinSDA,Pin.PULL_UP,Pin.OPEN_DRAIN)
+        self.pinSCK = pinSCK
+        self.pinSDA = pinSDA
+        self.address = address
+        self.i2c=I2C(scl=self.pinSCK, sda=self.pinSDA, freq=freq)
     
-def getHYT(i2c=i2c, verbosity=1):
-    """from: AHHYTM_E2.3.6"""
-    # setOn() # in case, it's switched off during pause
-    utime.sleep(.15)
-    i2c.readfrom_mem(0x28,0x1c,2)  # needed to initialize the next data measuring
-    utime.sleep(.01)
-    r = i2c.readfrom(HYT_Addr,4)
-    setOff();
-    if verbosity:
-        print(binascii.hexlify(r,' '))
-    status=r[0]>>6
-    h=r[0]&0b111111
-    h<<=8
-    h|=r[1]
-    h*=(100/(2**14-1))
-    t=(r[2]<<6) | (r[3]>>2)
-    t*=(165/16383)
-    t-=40
-    return Result(status,h,t)
+    def __str__(self):
+        return self.__unicode__()
+        
+    def __unicode__(self):
+        s,h,t = self.getValues()
+        return ("Status, Luftfeuchte, Temperatur: "
+                +str(s)+"/"+str(h)+"/"+str(t))
+  
+    def getValues(self):
+        """from: AHHYTM_E2.3.6"""
+        utime.sleep(.15)
+        self.i2c.readfrom_mem(self.address, 0x1c,2)  # needed to initialize the next data measuring
+        utime.sleep(.01)
+        r = self.i2c.readfrom(self.address, 4)
+        if self.verbosity:
+            print(binascii.hexlify(r,' '))
+        status=r[0]>>6
+        h=r[0]&0b111111
+        h<<=8
+        h|=r[1]
+        h*=(100/(2**14-1))
+        t=(r[2]<<6) | (r[3]>>2)
+        t*=(165/16383)
+        t-=40
+        return Result(status,h,t)
 
-
-# init i2c interface
-# i2c=I2C(scl=p5, sda=p4, freq=int(1e3))
-# i2c2=I2C(scl=p5, sda=p4, freq=int(1e3), timeout=int(10e3))
-# setOn() 
-
-def scan(i2ce=i2c):
-    devs = i2c.scan()
-    print("I2C devices found: ",devs, " = ", binascii.hexlify(bytes(devs)," "))
-    return devs
+    def scan(self):
+        devs = self.i2c.scan()
+        if self.verbosity:
+            print("I2C devices found: ",devs, " = ", 
+                    binascii.hexlify(bytes(devs)," "))
+        return devs
     
 if __name__ == "__main__":
     print("HYT221 Version:"+__version__)
-    scan()
-    hyt = getHYT(i2c, verbosity=0)
-    print("Status, Luftfeuchte, Temperatur: ",hyt.status, hyt.hygro, hyt.thermo)
+    sensor = HYT221(5,4)
+    sensor.scan()
+    hyt = sensor.getValues()
+    print("Status, Luftfeuchte, Temperatur: ",
+          hyt.status, hyt.humidity, hyt.temperature)
     # or simply:
     print(hyt)
     
-    
+# eof
