945ab584ef06ae8aaff3b260f1fdd9f1f0e765df
rundekugel <rundekugel@gmx.de>
2025-05-10 12:53:22 +0200

    send alarm only, if not again online after small time

--- Software/www-server/watchTimeout.py
+++ Software/www-server/watchTimeout.py
@@ -10,11 +10,54 @@ import ssl
 import urllib
 import json
 
-topics_sub = [("slw/gwx/v1/tele/LWT",0),("slw/gwx/2/tele/LWT",0),("slw/gwx/nous/+/tele/LWT",0)]
+topics_sub = [
+    # ("slw/gwx/v1/tele/LWT",0),("slw/gwx/2/tele/LWT",0),("slw/gwx/nous/+/tele/LWT",0),
+("slw/gwx/test/+/LWT",0)
+    ]
+
+class Delayer:
+    topic=""
+    lwl=""
+    startTime=None
+    DESTROY_ME = -1
+    info = ""
+    msg = mclient.MQTTMessage
+    def __init__(self, mqttmsg :mclient.MQTTMessage, topic=None,lwl=None,  info=None):
+        self.startTime = time.time()
+        if mqttmsg:
+            self.msg = mqttmsg
+            self.topic = mqttmsg.topic
+            self.lwl = str(mqttmsg.payload)
+        if topic:
+            self.topic = topic
+        if lwl:
+            self.lwl = lwl
+        if info is not None:
+            self.info = info
+        else:
+            self.info = self.topic + " " + str(self.lwl)
+
+    def update(self, lwl):
+        if globs.verbosity>1:
+            print(self.__dict__)
+            print(self.msg.timestamp)
+        if self.lwl == "Offline":
+            if lwl =="Online":
+                if time.time() < self.startTime + globs.msgdelaytime:
+                    self.startTime = self.DESTROY_ME
+                    return
+        startTime = time.time()
+        self.lwl = lwl
+        self.info = self.getFullMsg()
+    def getFullMsg(self):
+        return self.topic + " " + str(self.lwl)
+
 
 class globs:
   cfg = {"verbosity":3}
-
+  delayers=[]
+  msgdelaytime = 20
+  verbosity = 1
 
 def on_connect(client, userdata, flags, rc):
     res=["ok","?","?","?","?","Auth error"]
@@ -26,8 +69,20 @@ def on_message(client, userdata, msg):
         msg.payload = msg.payload.decode()
     info = msg.topic + " " + str(msg.payload)
     print(info)
-    rx = globs.cfg.get("signalReceivers")
-    signalTx(info, rx)
+    r=1  # globs.delayers.get(msg.topic)
+    delayer = delayer_find(msg.topic)
+    if delayer:
+        delayer.update(str(msg.payload))
+    else:
+        d = Delayer(msg)
+        globs.delayers.append(d)
+
+def delayer_find(topic):
+    delayer: Delayer
+    for delayer in globs.delayers:
+        if delayer.topic==topic:
+            return delayer
+    return None
 
 def telegramTx(info, chatid="112350312"):    
   user="bot237720890"
@@ -105,6 +160,44 @@ client.on_message = on_message
 client.tls_set(tls_version=mclient.ssl.PROTOCOL_TLS)
 client.tls_insecure_set(False)
 
-client.connect(server, port, 60)
-client.loop_forever()
+client.connect(server, port, 90)
+    # client.loop_forever()
+client.loop_start()
+dorun = 1
+while dorun:
+    if client.is_connected():
+        dorun =2
+        try:
+            # client.loop(timeout=2)
+            pass
+        except Exception as e:
+            dorun = 1
+            print(e)
+    else:
+        if dorun==2:
+            dorun=0
+    if globs.verbosity>1:
+        print("."+str(dorun), end="")
+    delayer: Delayer
+
+    for delayer in globs.delayers:
+        if globs.verbosity>1:
+            print(delayer.__dict__)
+        if delayer.startTime == Delayer.DESTROY_ME:
+            #globs.delayers.pop(i)
+            globs.delayers.remove(delayer)
+            continue
+        if time.time() >= delayer.startTime +globs.msgdelaytime:
+            # delayer.lwl = Delayer.DESTROY_ME
+            try:
+                info = delayer.info
+                globs.delayers.remove(delayer)
+                rx = globs.cfg.get("signalReceivers")
+                if globs.verbosity:
+                    print("Alarm!",delayer.info, rx)
+                signalTx(delayer.info, rx)
+            except Exception as e:
+                print(e)
+    time.sleep(1)
+client.loop_stop()
 print("fin.")
