089971dcfea2b8e48261176764a708b0c7bddada
rundekugel <rundekugel@gmx.de>
2026-06-04 22:49:18 +0200

    V0.0.3 json konfig upload/download

--- Software/www-server/config.php
+++ Software/www-server/config.php
@@ -117,6 +117,13 @@ function readTable(id){
 }
 
 function getTable(id){
+
+    var test = document.getElementById(id + "_0_0");
+
+    if(!test){
+        return "";
+    }
+
     var x=12;
     var y=24;
     var data = "";
@@ -124,12 +131,14 @@ function getTable(id){
     for (var y1 = 0; y1 < y; y1++) {
         for (var x1 = 0; x1 < x; x1++) {
             var cid = id+"_"+x1+"_"+y1;
-            var v = document.getElementById(cid).textContent;
-
-            if(v != "x") v=".";
-            data += v;
+            var cell = document.getElementById(cid);
+            if(cell && cell.textContent=="x"){
+                data += "x";
+            }else{
+                data += ".";
+            }
         }
-        data +=";";
+        data += ";";
     }
 
     return data;
@@ -169,6 +178,191 @@ function downloadConfig_1u2(){
 
     download('gwxHausWasserKonfig_' + timetext + '.txt', data);
 }
+
+function setTable(id, data){
+    var rows = data.split(";");
+
+    for(var y=0; y<24; y++){
+
+        if(!rows[y]) continue;
+
+        for(var x=0; x<12; x++){
+
+            var cid = id + "_" + x + "_" + y;
+            var cell = document.getElementById(cid);
+
+            if(!cell) continue;
+
+            if(rows[y][x] == "x"){
+                cell.textContent = "x";
+            }else{
+                cell.textContent = ".";
+            }
+        }
+    }
+}
+
+function exportConfigJSON(){
+
+    var cfg = {
+
+        version : 2,
+        created : new Date().toISOString(),
+
+        water : {
+            w1 : getTable("table1"),
+            w2 : getTable("table2"),
+            w3 : getTable("table3"),
+            w4 : getTable("table4")
+        },
+
+        haus1 : {
+            tmax     : getValue("h1tmax"),
+            tmin     : getValue("h1tmin"),
+            hmax     : getValue("h1hmax"),
+            frost    : getValue("h1frost"),
+            hmin1    : getValue("h1hmin1"),
+            hmin2    : getValue("h1hmin2"),
+            heizung  : getValue("h1heizung"),
+
+            reverse1 : getChecked("h1R"),
+            reverse2 : getChecked("h1R2")
+        },
+
+        haus2 : {
+            tmax     : getValue("h2tmax"),
+            tmin     : getValue("h2tmin"),
+            hmax     : getValue("h2hmax"),
+            frost    : getValue("h2frost"),
+            hmin1    : getValue("h2hmin1"),
+            hmin2    : getValue("h2hmin2"),
+            heizung  : getValue("h2heizung"),
+
+            reverse1 : getChecked("h2R"),
+            reverse2 : getChecked("h2R2")
+        }
+    };
+
+    var currentdate = new Date();
+
+    var filename =
+        "gwxHausKonfig_" +
+        currentdate.getFullYear() +
+        ("0"+(currentdate.getMonth()+1)).slice(-2) +
+        ("0"+currentdate.getDate()).slice(-2) + "_" +
+        ("0"+currentdate.getHours()).slice(-2) +
+        ("0"+currentdate.getMinutes()).slice(-2) +
+        ("0"+currentdate.getSeconds()).slice(-2) +
+        ".json";
+
+    download(filename, JSON.stringify(cfg,null,2));
+}
+
+function importConfigJSON(){
+
+    var input = document.createElement("input");
+
+    input.type = "file";
+    //input.accept = ".json,application/json";
+
+    input.onchange = function(event){
+
+        var file = event.target.files[0];
+
+        if(!file){
+            return;
+        }
+
+        var reader = new FileReader();
+
+        reader.onload = function(e){
+
+            try{
+
+                var cfg = JSON.parse(e.target.result);
+
+                console.log(cfg);
+
+                if(cfg.water){
+
+                    if(cfg.water.w1) setTable("table1", cfg.water.w1);
+                    if(cfg.water.w2) setTable("table2", cfg.water.w2);
+                    if(cfg.water.w3) setTable("table3", cfg.water.w3);
+                    if(cfg.water.w4) setTable("table4", cfg.water.w4);
+
+                }else{
+
+                    // altes Format
+
+                    if(cfg.w1) setTable("table1", cfg.w1);
+                    if(cfg.w2) setTable("table2", cfg.w2);
+                    if(cfg.w3) setTable("table3", cfg.w3);
+                    if(cfg.w4) setTable("table4", cfg.w4);
+                }
+
+                if(cfg.haus1){
+
+                    setValue("h1tmax", cfg.haus1.tmax);
+                    setValue("h1tmin", cfg.haus1.tmin);
+                    setValue("h1hmax", cfg.haus1.hmax);
+                    setValue("h1frost", cfg.haus1.frost);
+                    setValue("h1hmin1", cfg.haus1.hmin1);
+                    setValue("h1hmin2", cfg.haus1.hmin2);
+                    setValue("h1heizung", cfg.haus1.heizung);
+
+                    setChecked("h1R", cfg.haus1.reverse1);
+                    setChecked("h1R2", cfg.haus1.reverse2);
+                }
+
+                if(cfg.haus2){
+
+                    setValue("h2tmax", cfg.haus2.tmax);
+                    setValue("h2tmin", cfg.haus2.tmin);
+                    setValue("h2hmax", cfg.haus2.hmax);
+                    setValue("h2frost", cfg.haus2.frost);
+                    setValue("h2hmin1", cfg.haus2.hmin1);
+                    setValue("h2hmin2", cfg.haus2.hmin2);
+                    setValue("h2heizung", cfg.haus2.heizung);
+
+                    setChecked("h2R", cfg.haus2.reverse1);
+                    setChecked("h2R2", cfg.haus2.reverse2);
+                }
+
+                alert("Konfiguration geladen.");
+
+            }catch(ex){
+
+                console.error(ex);
+                alert("Ungültige JSON-Datei.");
+            }
+        };
+
+        reader.readAsText(file);
+    };
+
+    input.click();
+}
+
+function getValue(id){
+    var e = document.getElementById(id);
+    return e ? e.value : "";
+}
+
+function setValue(id,value){
+    var e = document.getElementById(id);
+    if(e) e.value = value;
+}
+
+function getChecked(id){
+    var e = document.getElementById(id);
+    return e ? e.checked : false;
+}
+
+function setChecked(id,value){
+    var e = document.getElementById(id);
+    if(e) e.checked = value;
+}
+
 </script>
 </head>
 
@@ -176,7 +370,7 @@ function downloadConfig_1u2(){
 
 <h1>Konfiguration Gew&auml;chshaus Unter&ouml;d</h1>
 <hr>
-Test Version 0.0.2
+Test Version 0.0.3
 <hr>
 
 <a href="gwxhaus.php">Zurück</a>
@@ -207,15 +401,13 @@ if(isset($_SESSION["user"])) {
 <button onclick=settime()>Setzte Uhrzeit auf aktuelle Zeit</button>
 <h3>Gew&auml;chshaus Wasser Zeiten</h3>
 . = Aus / x = An<br>
-<table><td>
+<table><tr><td>
 <h4>Wasserventil1</h4><div id="table1">-</div></td><td></td><td>
-<h4>Wasserventil2</h4><div id="table2">-</div></td>
-</table>
+<h4>Wasserventil2</h4><div id="table2">-</div></td></tr></table>
 <br>
-<table><td>
+<table><tr><td>
 <h4>Wasserventil3</h4><div id="table3">-</div></td><td></td><td>
-<h4>Wasserventil4</h4><div id="table4">-</div></td>
-</table>
+<h4>Wasserventil4</h4><div id="table4">-</div></td></tr></table>
 <br>
 <hr>
 <H1>Haus1</H1>
@@ -243,15 +435,30 @@ Heizung2 unter <input type="text" id="h2heizung" name="h2heizung" value="8" size
 <button onclick='downloadConfig_1u2();'>Download Wasserzeiten</button><br>
 
 <form onsubmit="download(this['name'].value, this['text'].value)">
-  <input type="text" name="name" value="gwxHausWasserKonfig.txt" hidden="true">
+  <input type="hidden" name="name" value="gwxHausWasserKonfig.txt">
   <textarea name="text" hidden="true">leer</textarea>
   <input type="submit" value="Download" hidden="true">
 </form>
+
+<hr>
+
+<h3>JSON Import / Export</h3>
+
+
+<button onclick="importConfigJSON()">
+Konfiguration laden
+</button>
+
+<button onclick="exportConfigJSON()">
+Konfiguration speichern
+</button>
+
+<br><br>
 
 <div id="log">-</div>
 <hr>
 Daten werden bei der Übertragung verschlüsselt. Aktionen können nur nach Login durchgeführt werden.<br>
 Datenschutz: <a href="/Datenschutz.html">Hier klicken.</a><br>
-20260604-1
+20260604-2
 </body>
 
